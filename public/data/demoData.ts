cf1582d3bc287f6a42159f151b7a88ab2e39a88e
Khaled Ayouch <91567267+devkhaledd@users.noreply.github.com>
2023-08-24 12:57:58 +0300

    create demo data file

---
+++ public/data/demoData.ts
@@ -0,0 +1,23 @@
+const articles = [
+    {
+      title: 'Introduction to React',
+      date: 'August 15, 2023',
+      content:
+        'React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of the application efficiently. With React, you can use a declarative syntax to describe how your UI should look based on the current state of the data. This approach makes it easier to reason about your UI and helps in building interactive and dynamic web applications.',
+    },
+    {
+      title: 'Getting Started with Node.js',
+      date: 'August 20, 2023',
+      content:
+        'Node.js is a runtime environment that allows developers to execute JavaScript code on the server side. It provides an event-driven, non-blocking architecture for building scalable and fast applications. With Node.js, you can handle I/O operations efficiently and build real-time applications using technologies like WebSockets. Its vast ecosystem of packages and modules, available through npm, makes it a popular choice for backend development.',
+    },
+    {
+      title: 'CSS Flexbox Layout',
+      date: 'August 25, 2023',
+      content:
+        'Flexbox is a layout model in CSS that allows you to design flexible and responsive layouts with a single-dimensional layout approach. It makes it easier to distribute space and align content within containers. With flex properties like `flex-direction`, `justify-content`, and `align-items`, you can create complex layouts that adapt to different screen sizes. Whether you are building a simple card layout or a complex navigation menu, Flexbox provides a powerful toolset for designing modern web interfaces.',
+    },
+  ];
+  
+  export default articles;
+  
